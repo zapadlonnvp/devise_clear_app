36ca2441f2b9a9a7634097d19b90f3e77aa9e9fa
zapadlonnvp <zapadlon@gmail.com>
2020-04-03 22:37:58 +0300

    lesson 57 initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -127,8 +127,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['app165013698@heroku.com'],
-      :password       => ENV['rk1irpf73564'],
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
