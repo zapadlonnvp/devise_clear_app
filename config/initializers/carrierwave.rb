40a1e9eaba2dc62b9d17cbf30017537923fb27c5
zapadlonnvp <zapadlon@gmail.com>
2020-04-03 17:32:56 +0300

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,9 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
         provider:              'AWS',
-        aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-        aws_secret_access_key: ENV['S3_SECRET_KEY'],
-        region: 'us-east-2'
+        aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+        aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+        region: ENV['S3_REGION']
     }
 
     config.fog_directory  = ENV['S3_BUCKET_NAME']
