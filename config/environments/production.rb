d5d8c8c31f1810e4d75dfc5bbe14cd6b93153e3f
zapadlonnvp <zapadlon@gmail.com>
2020-04-04 16:00:34 +0300

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,12 +124,12 @@ Rails.application.configure do
 
   # А это для SendGrid
   ActionMailer::Base.smtp_settings = {
-      :domain         => 'heroku.com',
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :enable_starttls_auto => true
+      domain:         'heroku.com',
+      address:        'smtp.sendgrid.net',
+      port:           '587',
+      authentication: :plain,
+      user_name:      ENV['SENDGRID_USERNAME'],
+      password:       ENV['SENDGRID_PASSWORD'],
+      enable_starttls_auto: :true
   }
 end
