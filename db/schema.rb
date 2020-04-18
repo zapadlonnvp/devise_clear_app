71dc587b6de20a28e8301db0e4594d8892561808
zapadlonnvp <zapadlon@gmail.com>
2020-04-18 21:50:13 +0300

    no comments from self

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_110958) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "avatar"
+    t.string "avatar", default: "user.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
