2eec8a18b95fa33f90e711f4431862ad33dbdac5
zapadlonnvp <zapadlon@gmail.com>
2020-04-21 22:10:00 +0300

    lesson 57-2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@
 # Модель Коммента
 class Comment < ActiveRecord::Base
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates  :event, presence: true
   validates :body, presence: true
