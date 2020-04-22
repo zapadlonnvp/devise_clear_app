52460c23275c381d1008d4bbdaaea98eeb6813a5
zapadlonnvp <zapadlon@gmail.com>
2020-04-22 15:18:44 +0300

    lesson 57-2

--- db/migrate/20200422120244_change_column_null.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNull < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :comments, :user_id, true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_120244) do
+ActiveRecord::Schema.define(version: 2020_04_03_110958) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
